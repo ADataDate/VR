a029c0a7eedb91be171f9c96a5ddc8c41c60a202
unknown <mwest.lazerade@gmail.com>
2020-09-18 09:32:00 -0600

    dirtDevilLow in good state

--- intro_to_vr_fall2020/HW3/HW3_dirt_devil_manager/Assets/DirtDevil_manager.cs
+++ intro_to_vr_fall2020/HW3/HW3_dirt_devil_manager/Assets/DirtDevil_manager.cs
@@ -18,7 +18,8 @@ public class DirtDevil_manager : MonoBehaviour
     public float thetaOffset = 1; //
     public float spinSpeedOffset = 10;
     public float expandOffset = 0;
-    public float sizeOffset = 0; 
+    public float sizeOffset = 0;
+    public float riseSpeedOffset = 1;
 
 
  
@@ -36,14 +37,12 @@ public class DirtDevil_manager : MonoBehaviour
             devils[i] = Instantiate(devil, container.transform);
             devils[i].transform.GetChild(0).transform.localPosition = new Vector3(i * 0.01f, i * 0.1f, 0);
             devils[i].GetComponent<DirtDevil_controller>().theta = UnityEngine.Random.Range(0, 360);
-            devils[i].GetComponent<DirtDevil_controller>().spinSpeed = spinSpeedOffset + UnityEngine.Random.Range(-5, 5);
+            devils[i].GetComponent<DirtDevil_controller>().spinSpeed = UnityEngine.Random.Range(spinSpeedOffset + spinSpeedOffset * .5f, spinSpeedOffset - spinSpeedOffset * .5f);
             devils[i].GetComponent<DirtDevil_controller>().height = devilHeight + UnityEngine.Random.Range(devilHeight+devilHeight*.3f, devilHeight - devilHeight * .3f);
             devils[i].GetComponent<DirtDevil_controller>().rise = UnityEngine.Random.Range(0.0f, devils[i].GetComponent<DirtDevil_controller>().height);
             devils[i].GetComponent<DirtDevil_controller>().expandSpeed = expandOffset + UnityEngine.Random.Range(expandOffset + expandOffset * .1f, expandOffset - expandOffset * .1f);
             devils[i].GetComponent<DirtDevil_controller>().size = sizeOffset + UnityEngine.Random.Range(sizeOffset + sizeOffset * .1f, sizeOffset - sizeOffset * .5f);
-
-
-
+            devils[i].GetComponent<DirtDevil_controller>().riseSpeed = riseSpeedOffset + UnityEngine.Random.Range(riseSpeedOffset + riseSpeedOffset * .1f, riseSpeedOffset - riseSpeedOffset * .5f);
         }
     }
 
